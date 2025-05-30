285ff1a679e6c78ce87e35e17915d6bde1bff470
JooseF57 <2236000408@utna.edu.mx>
2025-05-29 18:39:31 -0600

    Agregamos el js y eliminamos lineas que no son importantes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function ($) {
     "use strict";
     
-    // Dropdown on mouse hover
+
     $(document).ready(function () {
         function toggleNavbarMethod() {
             if ($(window).width() > 992) {
@@ -19,7 +19,7 @@
     });
     
     
-    // Back to top button
+
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
             $('.back-to-top').fadeIn('slow');
@@ -33,7 +33,7 @@
     });
 
 
-    // Date and time picker
+   
     $('.date').datetimepicker({
         format: 'L'
     });
